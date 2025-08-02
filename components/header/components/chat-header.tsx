916992e8632114192265d6b7fbbd4e87c4af3d40
Y <yhwh@practice.energy>
2025-08-02 07:37:55 +0300

    Finetune

--- components/header/components/chat-header.tsx
+++ components/header/components/chat-header.tsx
@@ -22,23 +22,27 @@ export const ChatHeader = ({
                            }: ChatHeaderProps) => {
     return (
         <header className="fixed top-0 left-0 right-0 h-16 bg-white opacity-70 z-50 px-4 flex items-center justify-between">
-            <SidebarToggleButton
-                toggleSidebar={toggleSidebar}
-                className="mr-2"
-            />
+            <div className="w-min">
+                <SidebarToggleButton
+                    toggleSidebar={toggleSidebar}
+                    className="mr-2 w-min"
+                />
+            </div>
 
             {/* Центральная часть - название чата */}
-            <div className="flex-1 text-center font-medium text-gray-900 truncate line-clamp-1 opacity-100">
+            <div className="flex-1 text-center font-medium text-gray-900 truncate line-clamp-1 opacity-100 w-full">
                 {currentChat?.description || currentChat?.title || "Чат"}
             </div>
 
-            <ProfileIcon
-                isAuthenticated={isAuthenticated}
-                toggleProfileMenu={toggleProfileMenu}
-                user={user}
-                className="ml-4"
-                iconSize={50}
-            />
+            <div className="ml-auto w-min">
+                <ProfileIcon
+                    isAuthenticated={isAuthenticated}
+                    toggleProfileMenu={toggleProfileMenu}
+                    user={user}
+                    className="ml-4"
+                    iconSize={50}
+                />
+            </div>
         </header>
     )
 }
