5beb6b71e367176f9f1ef40b9b5b844ebe0589ec
Y <yhwh@practice.energy>
2025-08-21 22:00:19 +0300

    Finetune

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,17 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig();
+export default defineCloudflareConfig({
+  experimental: {
+    staticGeneration: {
+      prerenderRoutes: ["/"]
+    }
+  },
+  override: {
+    wrapper: "cloudflare-pages",
+    converter: "edge",
+    tagCache: "dummy",
+    queue: "dummy",
+    incrementalCache: "dummy",
+    imageOptimization: "dummy"
+  }
+});
